97a5252eb4d9948fbda0d36c023de0ba4bc54242
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-05-22 00:04:39 -0600

    Hook up the twilio and lex libraries to the lambda handler

--- twilio-webhook/src/twilio_webhook/handler.clj
+++ twilio-webhook/src/twilio_webhook/handler.clj
@@ -1,12 +1,11 @@
 (ns twilio-webhook.handler
-  (:require
-    [cheshire.core :refer [generate-stream parse-stream]]
-    [clojure.java.io :as io]
-    [clojure.walk :refer [keywordize-keys]]
-    [ring.util.codec :refer [form-decode]]
-    [taoensso.timbre :as timbre :refer [info]]
-    [twilio-webhook.lex :refer [lex text-request sms]]
-    [twilio-webhook.twilio :refer [create-sms]])
+  (:require [cheshire.core :refer [generate-stream parse-stream]]
+            [clojure.java.io :as io]
+            [clojure.walk :refer [keywordize-keys]]
+            [ring.util.codec :refer [form-decode]]
+            [taoensso.timbre :as timbre :refer [info]]
+            [twilio-webhook.lex :as lex]
+            [twilio-webhook.twilio :as twilio])
   (:import (java.io InputStream OutputStream)
            (com.amazonaws.services.lambda.runtime Context))
   (:gen-class
@@ -20,17 +19,27 @@
               input input-stream
               writer (io/writer output)
               reader (io/reader input)]
+    (let [request (-> reader
+                      (parse-stream true)
+                      :body
+                      form-decode
+                      keywordize-keys)
+          message (:Body request)
+          from (:From request)]
 
-    (info "Incoming message:"
-          (-> reader
-              (parse-stream true)
-              :body
-              form-decode
-              keywordize-keys
-              :Body))
+      (info "Incoming message from:" from message)
 
-    (generate-stream {:isBase64Encoded false
-                      :statusCode      200
-                      :headers         {:Content-Type "application/xml"}
-                      :body            (create-sms "twix!")}
-                     writer)))
+      (generate-stream {:isBase64Encoded false
+                        :statusCode      200
+                        :headers         {:Content-Type "application/xml"}
+                        :body            (twilio/create-sms
+                                           (.getMessage (lex/send-text-request from message)))}
+                       writer))))
+
+(comment
+  (DONE isolate the behaviour that is needed to proxy SMS to Lex)
+  (DONE move all http-server code to dev)
+  (DONE create AWS Service Proxy Lambda handler)
+
+  (date 2017-05-22T00:03:22.331Z)
+  (TODO determine when an MMS is received, and save to a bucket))
