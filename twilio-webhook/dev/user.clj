4c8602079a596611d3de392a23c205370eb664d6
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-05-21 23:17:38 -0600

    Remove all dev helpers
    Need to rethink local development server, for now removing as it is all
    broken.

--- twilio-webhook/dev/user.clj
+++ twilio-webhook/dev/user.clj
@@ -1,23 +1,14 @@
 (ns user
   (:require [clojure.tools.namespace.repl :refer [refresh]]
             [org.httpkit.server :as http]
-            [ring.middleware.reload :refer [wrap-reload]]
-            [twilio-webhook.handler :refer [app-routes]]))
+            [ring.middleware.defaults :refer [api-defaults wrap-defaults]]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (def server nil)
 
-(defn start []
-  (let [app (wrap-reload #'app-routes)
-        port 3000]
-    (alter-var-root #'server
-                    (constantly (http/run-server app {:port port})))
-    (println (str "Listening on port " port "!"))))
+(defn start [])
 
-(defn stop []
-  (alter-var-root #'server
-                  (fn [s]
-                    (when s
-                      (s :timeout 100)))))
+(defn stop [])
 
 (defn restart []
   (stop)
