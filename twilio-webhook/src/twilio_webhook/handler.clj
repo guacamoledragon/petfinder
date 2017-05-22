064128038ee1846c3f3e80e1ba3039ec84fc0780
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-05-22 00:25:57 -0600

    Remove invalid Lex userId characters

--- twilio-webhook/src/twilio_webhook/handler.clj
+++ twilio-webhook/src/twilio_webhook/handler.clj
@@ -5,7 +5,8 @@
             [ring.util.codec :refer [form-decode]]
             [taoensso.timbre :as timbre :refer [info]]
             [twilio-webhook.lex :as lex]
-            [twilio-webhook.twilio :as twilio])
+            [twilio-webhook.twilio :as twilio]
+            [clojure.string :as str])
   (:import (java.io InputStream OutputStream)
            (com.amazonaws.services.lambda.runtime Context))
   (:gen-class
@@ -25,7 +26,7 @@
                       form-decode
                       keywordize-keys)
           message (:Body request)
-          from (:From request)]
+          from (str/replace (:From request) #"\+" "")]
 
       (info "Incoming message from:" from message)
 
@@ -41,5 +42,5 @@
   (DONE move all http-server code to dev)
   (DONE create AWS Service Proxy Lambda handler)
 
-  (date 2017-05-22T00:03:22.331Z)
+  (date "2017-05-22T00:03:22.331Z")
   (TODO determine when an MMS is received, and save to a bucket))
