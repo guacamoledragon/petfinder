254a6f377ee6cc963ae91723ac1f2d1d16920b8c
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-05-21 23:19:12 -0600

    Implement handler fn as a Lambda API Gateway Proxy entrypoint
    The entrypoint of the application is a Lambda endpoint, it is now doing
    such a different thing so it's pretty much a rewrite of the
    functionality.

--- twilio-webhook/src/twilio_webhook/handler.clj
+++ twilio-webhook/src/twilio_webhook/handler.clj
@@ -1,11 +1,44 @@
 (ns twilio-webhook.handler
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+  (:require
+    [cheshire.core :refer [generate-stream parse-stream]]
+    [clojure.java.io :as io]
+    [clojure.walk :refer [keywordize-keys]]
+    [ring.util.codec :refer [form-decode]]
+    [taoensso.timbre :as timbre :refer [info]]
+    [twilio-webhook.lex :refer [lex text-request sms]])
+  (:import (java.io InputStream OutputStream)
+           (com.amazonaws.services.lambda.runtime Context)
+           (com.twilio.twiml Body Message$Builder MessagingResponse$Builder))
+  (:gen-class
+    :methods [^:static [handler [java.io.InputStream java.io.OutputStream com.amazonaws.services.lambda.runtime.Context] void]]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+(defn ->twiml
+  [message]
+  (-> (MessagingResponse$Builder.)
+      (.message (-> (Message$Builder.)
+                    (.body (Body. message))
+                    .build))
+      .build))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(defn -handler
+  [^InputStream input-stream ^OutputStream output-stream ^Context context]
+  (info "Executing function:" (.getFunctionName context))
+
+  (with-open [output output-stream
+              input input-stream
+              writer (io/writer output)
+              reader (io/reader input)]
+
+    (info "Incoming message:"
+          (-> reader
+              (parse-stream true)
+              :body
+              form-decode
+              keywordize-keys
+              :Body))
+
+    (generate-stream {:isBase64Encoded false
+                      :statusCode      200
+                      :headers         {:Content-Type "application/xml"}
+                      :body            (.toXml (->twiml "twix!"))}
+                     writer)))
