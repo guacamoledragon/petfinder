820238869ee4dd5a85fa790b9d5645cc9dfe16a7
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-05-27 19:10:56 -0600

    Add dummy methods that quickly test functionality ad-hoc

--- twilio-webhook/dev/user.clj
+++ twilio-webhook/dev/user.clj
@@ -1,8 +1,12 @@
 (ns user
-  (:require [clojure.tools.namespace.repl :refer [refresh]]
+  (:require [clojure.java.io :as io]
+            [clojure.tools.namespace.repl :refer [refresh]]
             [org.httpkit.server :as http]
             [ring.middleware.defaults :refer [api-defaults wrap-defaults]]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [twilio-webhook.handler :as webhook])
+  (:import (java.io ByteArrayOutputStream)
+           (com.amazonaws.services.lambda.runtime Context)))
 
 (def server nil)
 
@@ -13,3 +17,20 @@
 (defn restart []
   (stop)
   (refresh :after 'user/start))
+
+(def sample-input-stream
+  (io/input-stream "resources/api-gateway-proxy.json"))
+
+(def dummy-context
+  (reify
+    Context
+    (getFunctionName [this] "dummy")))
+
+(defn test-handler
+  []
+  (with-open [input sample-input-stream
+              output (ByteArrayOutputStream.)]
+    (webhook/-handler input output dummy-context)
+    (-> output
+        .toByteArray
+        String.)))
