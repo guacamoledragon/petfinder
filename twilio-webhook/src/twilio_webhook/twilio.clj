ee2922a0733f218d290c6ae21039983d3dd11d47
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-05-28 00:01:51 -0600

    Add sanitize function to help convert raw twilio requests

--- twilio-webhook/src/twilio_webhook/twilio.clj
+++ twilio-webhook/src/twilio_webhook/twilio.clj
@@ -1,6 +1,25 @@
 (ns twilio-webhook.twilio
-  (:require [taoensso.timbre :as timbre])
+  (:require [camel-snake-kebab.core :as csk]
+            [taoensso.timbre :as timbre])
   (:import (com.twilio.twiml Body Message$Builder MessagingResponse$Builder TwiMLException)))
+
+(defn- numberify
+  [m]
+  (let [f (fn [[k v]] (try [k (Integer/parseInt v)]
+                           (catch NumberFormatException _ [k v])))]
+    (into {} (map f m))))
+
+(defn- kebabize-keys
+  "Transforms all map keys from strings to kebabified keywords."
+  [m]
+  (let [f (fn [[^String k v]] [(csk/->kebab-case-keyword k) v])]
+    (into {} (map f m))))
+
+(defn sanitize
+  [twilio-request]
+  (-> twilio-request
+      kebabize-keys
+      numberify))
 
 (defn create-sms
   [body]
