bc4f0339d073c9be996af885c3e88c4c61bff561
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-05-29 10:37:28 -0600

    Remove unused dev dependencies and plugins

--- twilio-webhook/project.clj
+++ twilio-webhook/project.clj
@@ -12,8 +12,7 @@
                  [com.twilio.sdk/twilio "7.9.1"]
                  [org.clojure/clojure "1.8.0"]
                  [ring/ring-codec "1.0.1"]]
-  :plugins [[lein-clj-lambda "0.10.1"]
-            [lein-ring "0.9.7"]]
+  :plugins [[lein-clj-lambda "0.10.1"]]
   :ring {:handler twilio-webhook.handler/app}
   :lambda {"dev" [{:handler       "twilio-webhook.handler.handler"
                    :function-name "twilio-webhook"
@@ -22,13 +21,8 @@
                    :region        "us-east-1"
                    :s3            {:bucket     "petfinder.com"
                                    :object-key "lambda/twilio-webhook-0.1.0-SNAPSHOT.jar"}}]}
-  :profiles {:dev     {:dependencies [[compojure "1.5.1"]
-                                      [http-kit "2.2.0"]
-                                      [javax.servlet/servlet-api "2.5"]
-                                      [org.clojure/tools.namespace "0.2.11"]
-                                      [ring/ring-defaults "0.2.1"]
-                                      [ring/ring-devel "1.6.0"]
-                                      [ring/ring-mock "0.3.0"]]
-                       :source-paths ["dev"]}
+  :profiles {:dev     {:dependencies   [[org.clojure/tools.namespace "0.2.11"]]
+                       :resource-paths ["resources" "dev-resources"]
+                       :source-paths   ["dev"]}
              :uberjar {:aot      [twilio-webhook.handler]
                        :jvm-opts ["-Xmx1g"]}})
